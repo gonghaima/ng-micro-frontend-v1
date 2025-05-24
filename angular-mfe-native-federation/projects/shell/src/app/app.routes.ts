29ef8fb99266360599e0f54a10264462beabcc99
Steven Gong <gonghaima@hotmail.com>
2025-05-24 12:41:01 +1000

    update route in shell app & federation.manifest.json , to make MFE working together

--- angular-mfe-native-federation/projects/shell/src/app/app.routes.ts
+++ angular-mfe-native-federation/projects/shell/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent, pathMatch: 'full' },
+  {
+    path: 'products',
+    loadComponent: () =>
+      loadRemoteModule('products', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: '**',
+    component: AppComponent,
+  },
+];
